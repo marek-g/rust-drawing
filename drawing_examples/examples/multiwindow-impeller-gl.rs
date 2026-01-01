bd85628c7c960019d8416733f80946b69bf022c8
Marek G <marek-dev@yandex.com>
2026-01-01 13:58:46 +0100

    Add multiwindow-impeller-gl example

---
+++ drawing_examples/examples/multiwindow-impeller-gl.rs
@@ -0,0 +1,24 @@
+#![windows_subsystem = "windows"]
+
+use drawing_examples::setup_window;
+use drawing_impeller::ImpellerContextGl;
+use std::error::Error;
+
+use windowing_qt::{Application, ApplicationOptions};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let app = Application::new(
+        ApplicationOptions::new()
+            .with_title("Example: multiwindow-impeller-gl")
+            .with_opengl_stencil_bits(8)
+            .with_force_xwayland(true),
+    )
+    .unwrap();
+
+    let _gl_window1_rc = setup_window::<ImpellerContextGl>("Window 1");
+    let _gl_window2_rc = setup_window::<ImpellerContextGl>("Window 2");
+
+    app.message_loop();
+
+    Ok(())
+}
