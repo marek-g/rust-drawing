1e2b1ae6dd1e072646cd2af768bce2e909b4bdc4
Marek G <marek-dev@yandex.com>
2026-02-24 13:43:29 +0100

    Update examples

--- drawing_examples/src/common.rs
+++ drawing_examples/src/common.rs
@@ -270,31 +270,31 @@ fn draw<C: drawing_api::ContextGl>(gl_window: &mut GlWindow<C>, resources: &Reso
     let mut pb = C::ParagraphBuilder::new(&resources.fonts).unwrap();
     pb.push_style(("F1", 10.0, "#FFF"));
     pb.add_text("Hello World!! yyy ąęśżółw,. 01234567890 abcdefghijk ABCDEFGHIJK XYZ xyz");
-    let paragraph = pb.build().unwrap();
+    let paragraph = pb.build(f32::INFINITY).unwrap();
     dlb.draw_paragraph((350.0 + pos_y, 200.0), &paragraph);
 
     let mut pb = C::ParagraphBuilder::new(&resources.fonts).unwrap();
     pb.push_style(("F1", 12.0, "#FFF"));
     pb.add_text("Hello World!! yyy ąęśżółw,.\n01234567890 abcdefghijk ABCDEFGHIJK XYZ xyz");
-    let paragraph = pb.build().unwrap();
+    let paragraph = pb.build(f32::INFINITY).unwrap();
     dlb.draw_paragraph((350.0, 220.0 - pos_y), &paragraph);
 
     let mut pb = C::ParagraphBuilder::new(&resources.fonts).unwrap();
     pb.push_style(("F1", 14.0, "#FFF"));
     pb.add_text("Hello World!! yyy ąęśżółw,.\n01234567890 abcdefghijk ABCDEFGHIJK XYZ xyz");
-    let paragraph = pb.build().unwrap();
+    let paragraph = pb.build(f32::INFINITY).unwrap();
     dlb.draw_paragraph((350.0 - pos_y, 240.0 + pos_y * 2.0), &paragraph);
 
     let mut pb = C::ParagraphBuilder::new(&resources.fonts).unwrap();
     pb.push_style(("F1", 16.0, "#FFF"));
     pb.add_text("Hello World!! yyy ąęśżółw,. 01234567890 abcdefghijk ABCDEFGHIJK XYZ xyz");
-    let paragraph = pb.build().unwrap();
+    let paragraph = pb.build(f32::INFINITY).unwrap();
     dlb.draw_paragraph((350.0 - pos_y, 260.0), &paragraph);
 
     let mut pb = C::ParagraphBuilder::new(&resources.fonts).unwrap();
     pb.push_style(("F1", 18.0, "#FFF"));
     pb.add_text("Hello World!! yyy ąęśżółw,. 01234567890 abcdefghijk ABCDEFGHIJK XYZ xyz");
-    let paragraph = pb.build().unwrap();
+    let paragraph = pb.build(f32::INFINITY).unwrap();
     dlb.draw_paragraph((350.0 + pos_y, 280.0 + pos_y), &paragraph);
 
     let mut pb = C::PathBuilder::default();
@@ -416,7 +416,7 @@ fn draw<C: drawing_api::ContextGl>(gl_window: &mut GlWindow<C>, resources: &Reso
     let mut pb = C::ParagraphBuilder::new(&resources.fonts).unwrap();
     pb.push_style(ParagraphStyle::simple("F1", 22.0, C::Paint::color("#FFF")));
     pb.add_text("Render target test");
-    let paragraph = pb.build().unwrap();
+    let paragraph = pb.build(f32::INFINITY).unwrap();
     dlb.draw_paragraph((207.0, 232.0), &paragraph);
 
     dlb.restore();
